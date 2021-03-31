6a7f8a334ca981ee505a99bb19a07b0307fc0c95
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2021-03-31 00:23:13 -0400

    Update

--- app/src/main/java/io/github/kwanyoon/androidapplication/MainActivity.java
+++ app/src/main/java/io/github/kwanyoon/androidapplication/MainActivity.java
@@ -3,6 +3,9 @@ package io.github.kwanyoon.androidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnClick(View view) {
+        // getting user input
+        EditText editTxtName = findViewById(R.id.editTxtName);
+        String nameInput = editTxtName.getText().toString();
+
+        // changing text on button click
+        TextView txtHello = findViewById(R.id.textView);
+        txtHello.setText("Hello " + nameInput);
     }
 }
